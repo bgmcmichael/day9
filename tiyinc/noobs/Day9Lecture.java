9b6857d9cc290e6cbde79aba935f7b4702574ee1
benjamin mcmichael <benjamin mcmichael>
2016-08-18 11:04:03 -0400

    base version of Day9Lecture

---
+++ tiyinc/noobs/Day9Lecture.java
@@ -0,0 +1,10 @@
+package tiyinc.noobs;
+
+/**
+ * Created by fenji on 8/18/2016.
+ */
+public class Day9Lecture {
+    public static void main(String[] args) {
+        System.out.println("running");
+    }
+}
