ac08cf6b37af7f22ac5f73000655971c1a4e7622
benjamin mcmichael <benjamin mcmichael>
2016-08-18 11:06:57 -0400

    going a bit overboard on commits to learn

--- tiyinc/noobs/Day9Lecture.java
+++ tiyinc/noobs/Day9Lecture.java
@@ -5,6 +5,13 @@ package tiyinc.noobs;
  */
 public class Day9Lecture {
     public static void main(String[] args) {
+        Day9Lecture myLecture = new Day9Lecture();
         System.out.println("running");
+
+        myLecture.testWriteFile();
+    }
+
+    public void testWriteFile() {
+        System.out.println("test write file");
     }
 }
